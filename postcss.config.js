69938b642e441fbea0e6de2a507e9021e17c9b7c
Jake Rayson <contact@growdigital.org>
2021-05-03 15:04:56 +0100

    update article page

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,11 @@
-module.exports = {
-	plugins: {
-		autoprefixer: {
-			browsers: [
-				"last 2 versions",
-				"Explorer >= 8",
-			]
-		}
-	},
-}
+// module.exports = {
+// 	plugins: {
+// 		autoprefixer: {
+// 			browsers: [
+// 				"last 2 versions",
+// 				"Explorer >= 8",
+// 			]
+// 		}
+// 	},
+// }
 
