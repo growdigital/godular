b472ed01319eea2e23625b6cdc94efe216dad521
Jake Rayson <contact@growdigital.org>
2021-05-11 21:53:37 +0100

    re-added minify

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,3 @@
-// module.exports = {
-// 	plugins: {
-// 		autoprefixer: {
-// 			browsers: [
-// 				"last 2 versions"
-// 			]
-// 		}
-// 	},
-// }
-
 module.exports = {
   plugins: {
     'postcss-css-variables': { },
